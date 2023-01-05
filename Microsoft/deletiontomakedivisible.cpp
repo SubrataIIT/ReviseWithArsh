bfdcb4e6b50437370f9e962d79a596ec0917c737
SubrataIIT <111726085+SubrataIIT@users.noreply.github.com>
2023-01-05 14:53:56 +0530

    Add files via upload

---
+++ Microsoft/deletiontomakedivisible.cpp
@@ -0,0 +1,26 @@
+  //Question link:https://leetcode.com/problems/minimum-deletions-to-make-array-divisible/
+  int minOperations(vector<int>& nums, vector<int>& numsD) {
+        int ans = -1;
+        int n = nums.size();
+       sort(nums.begin() , nums.end());
+        
+        int gcdd = *max_element(numsD.begin() , numsD.end());
+        
+        for(auto i:numsD)
+        {
+            gcdd = __gcd(gcdd , i);
+        }
+        
+        
+        cout<<gcdd<<"\n";
+        
+        for(int i=0;i<n;++i)
+        {
+            if(gcdd%nums[i] == 0)
+            {
+               ans = i;
+                break;
+            } 
+        }
+        return ans;
+    }
